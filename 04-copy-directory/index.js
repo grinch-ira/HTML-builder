8f468af8c831a4c25dad9e88e9b6144ec6d99c43
Hryniuk Iryna <grinch.ira@gmail.com>
2021-11-02 17:43:30 +0300

    feat: start 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const fsProm = require('fs/promises')
 
+const oldFolder = path.join(__dirname, 'files');
+const newFolder = path.join(__dirname, 'files-copy')
+
+async function copyDirectory(){
+
+}
