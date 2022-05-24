329fbbc9bb7f86f47f3580a1b9a7786945b847f6
Iryna Hryniuk <48655896+grinch-ira@users.noreply.github.com>
2022-05-24 11:39:02 +0300

    Update 01-read-file index.js

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,8 @@
 const fs = require('fs')
-const chalk = require('chalk')
 const path = require('path')
 
 
 let readabble = fs.createReadStream(path.join(__dirname, 'text.txt'),'utf8')
 readabble.on('data',(chunk)=>{
-    console.log(chalk(chunk))
+    console.log(chunk.toString())
 })
